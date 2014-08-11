415ebb22fdf1319cc1ceeb43241d809e3ce9912b
den1k <den1k@me.com>
2014-08-10 20:02:45 -0400

    Fix new-note styles, cursor position and prevent it from submitting an empty string'

--- app/components/new-note.js
+++ app/components/new-note.js
@@ -1,24 +1,30 @@
 import Ember from 'ember';
-
 import {actionOnKey} from 'whiteink/helpers/keyboard-actions';
 
 export default Ember.Component.extend({
 
   tagName: 'li',
-  classNames: ['new-note-field'],
+  classNames: 'new-note note'.w(),
   attributeBindings: ['contenteditable'],
   contenteditable: 'true',
 
   didInsertElement: function() {
-    this.$().focus();
+    // A hack to move the cursor on
+    // a contenteditable element w/
+    // text align: right; to the
+    // right - DH
+    this.$().focus().html('&nbsp;');
   },
 
   submitNote: function() {
     var component = this;
     actionOnKey(13, function() {
-      var newNote = component.$().text();
-      component.sendAction('action', newNote);
-      component.toggleProperty('parentView.willAddNote');
+      // .slice removes the '&nbsp;'
+      var newNote = component.$().text().slice(0, -1);
+      if (newNote.length) {
+        component.sendAction('action', newNote);
+        component.toggleProperty('parentView.willAddNote');
+      }
     });
   }.on('keyDown')
 
